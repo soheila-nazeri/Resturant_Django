7b90dc6ddf6cdbb1d03a3ca6445180956a16bfa1
nazeri <s.nazeri2009@gmail.com>
2023-10-23 23:29:31 +0300

    add static files

--- resturant_project/settings.py
+++ resturant_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'resturant_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= BASE_DIR/'static'
+STATICFILES_DIRS=['resturant_project/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
